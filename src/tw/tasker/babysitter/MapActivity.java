3c56b8b93dab67f2577f558f613938ef0fd3022e
soon530 <soon530@gmail.com>
2014-04-23 17:37:32 +0800

    定位按鈕出現在map上 OPEN - #5: 加入location功能

--- src/tw/tasker/babysitter/MapActivity.java
+++ src/tw/tasker/babysitter/MapActivity.java
@@ -1,17 +1,20 @@
 package tw.tasker.babysitter;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
 
 public class MapActivity extends ActionBarActivity {
+
+	private GoogleMap mMap;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +46,28 @@ public class MapActivity extends ActionBarActivity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+        setUpMapIfNeeded();
+
+	}
+
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                mMap.setMyLocationEnabled(true);
+                //mMap.setOnMyLocationButtonClickListener(this);
+            }
+        }
+    }
 
 	/**
 	 * A placeholder fragment containing a simple view.
