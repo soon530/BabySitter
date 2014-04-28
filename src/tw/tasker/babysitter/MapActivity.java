327789aaba33bc2c435988c50a9cd4bb48b957e3
soon530 <soon530@gmail.com>
2014-04-28 17:37:20 +0800

    新增LocationClient並與LocationServer連結 OPEN - #5: 加入location功能

--- src/tw/tasker/babysitter/MapActivity.java
+++ src/tw/tasker/babysitter/MapActivity.java
@@ -1,31 +1,44 @@
 package tw.tasker.babysitter;
 
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient.ConnectionCallbacks;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationClient;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 
-public class MapActivity extends ActionBarActivity {
+public class MapActivity extends ActionBarActivity implements
+		ConnectionCallbacks, OnConnectionFailedListener {
 
 	private GoogleMap mMap;
+	private LocationClient mLocationClient;
+	private Location mCurrentLocation;
+	private final static String MAP_ACTIVITY_TAG = "MapActivity";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.fragment_search_babysitter_map);
 
-/*		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
-*/	}
+		mLocationClient = new LocationClient(this, this, this);
+
+		/*
+		 * if (savedInstanceState == null) {
+		 * getSupportFragmentManager().beginTransaction() .add(R.id.container,
+		 * new PlaceholderFragment()).commit(); }
+		 */}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -46,28 +59,43 @@ public class MapActivity extends ActionBarActivity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-	
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+
+		mLocationClient.connect();
+	}
+
 	@Override
 	protected void onResume() {
-		// TODO Auto-generated method stub
 		super.onResume();
-        setUpMapIfNeeded();
+		setUpMapIfNeeded();
 
 	}
 
-    private void setUpMapIfNeeded() {
-        // Do a null check to confirm that we have not already instantiated the map.
-        if (mMap == null) {
-            // Try to obtain the map from the SupportMapFragment.
-            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
-                    .getMap();
-            // Check if we were successful in obtaining the map.
-            if (mMap != null) {
-                mMap.setMyLocationEnabled(true);
-                //mMap.setOnMyLocationButtonClickListener(this);
-            }
-        }
-    }
+	@Override
+	protected void onStop() {
+
+		mLocationClient.disconnect();
+
+		super.onStop();
+	}
+
+	private void setUpMapIfNeeded() {
+		// Do a null check to confirm that we have not already instantiated the
+		// map.
+		if (mMap == null) {
+			// Try to obtain the map from the SupportMapFragment.
+			mMap = ((SupportMapFragment) getSupportFragmentManager()
+					.findFragmentById(R.id.map)).getMap();
+			// Check if we were successful in obtaining the map.
+			if (mMap != null) {
+				mMap.setMyLocationEnabled(true);
+				// mMap.setOnMyLocationButtonClickListener(this);
+			}
+		}
+	}
 
 	/**
 	 * A placeholder fragment containing a simple view.
@@ -86,4 +114,61 @@ public class MapActivity extends ActionBarActivity {
 		}
 	}
 
+	// for location
+	@Override
+	public void onConnected(Bundle connectionHint) {
+		// TODO Auto-generated method stub
+		mCurrentLocation = getLocation();
+
+	}
+
+	private Location getLocation() {
+		// If Google Play Services is available
+		if (servicesConnected()) {
+			// Get the current location
+			return mLocationClient.getLastLocation();
+		} else {
+			return null;
+		}
+	}
+
+	private boolean servicesConnected() {
+		// Check that Google Play services is available
+		int resultCode = GooglePlayServicesUtil
+				.isGooglePlayServicesAvailable(this);
+
+		// If Google Play services is available
+		if (ConnectionResult.SUCCESS == resultCode) {
+			// if (Application.APPDEBUG) {
+				// In debug mode, log the status
+				Log.d(MAP_ACTIVITY_TAG, "Google play services available");
+			// }
+			// Continue
+			return true;
+			// Google Play services was not available for some reason
+		} else {
+			// Display an error dialog
+/*			Dialog dialog = GooglePlayServicesUtil.getErrorDialog(resultCode,
+					this, 0);
+			if (dialog != null) {
+				ErrorDialogFragment errorFragment = new ErrorDialogFragment();
+				errorFragment.setDialog(dialog);
+				errorFragment.show(getSupportFragmentManager(),
+						Application.APPTAG);
+			}*/
+			return false;
+		}
+	}
+
+	@Override
+	public void onDisconnected() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onConnectionFailed(ConnectionResult result) {
+		// TODO Auto-generated method stub
+
+	}
 }
