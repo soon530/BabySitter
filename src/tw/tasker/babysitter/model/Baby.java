caf5ada2675cee3c9196a7db97793efb5007d283
soon530 <soon530@gmail.com>
2014-05-22 17:09:26 +0800

    新增Baby物件 (data model for parse) OPEN - #14: [寶寶明細]導入Parse機制

---
+++ src/tw/tasker/babysitter/model/Baby.java
@@ -0,0 +1,41 @@
+package tw.tasker.babysitter.model;
+
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+@ParseClassName("baby")
+public class Baby extends ParseObject {
+
+	public String getName() {
+		String value = getString("name");
+		return value;
+	}
+
+	public void setName(String value) {
+		put("name", value);
+	}
+
+	public String getNote() {
+		String value = getString("note");
+		return value;
+	}
+
+	public void setNote(String value) {
+		put("note", value);
+	}
+	
+	public int getFavorite() {
+		int value = getInt("note");
+		return value;
+	}
+
+	public void setFavorite(int value) {
+		put("note", value);
+	}
+
+	public static ParseQuery<Baby> getQuery() {
+		return ParseQuery.getQuery(Baby.class);
+	}
+
+}
