f16d705893e46ca28012bf6e422646dedc834dc6
soon530 <soon530@gmail.com>
2014-05-23 22:17:28 +0800

    完成[我的收藏]列表功能 OPEN - #14: [寶寶明細]導入Parse機制

--- src/tw/tasker/babysitter/BabyListActivity.java
+++ src/tw/tasker/babysitter/BabyListActivity.java
@@ -1,15 +1,25 @@
 package tw.tasker.babysitter;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import tw.tasker.babysitter.model.Baby;
+import tw.tasker.babysitter.model.Favorite;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.display.RoundedBitmapDisplayer;
+import com.parse.ParseQuery;
+import com.parse.ParseQueryAdapter;
+import com.parse.ParseQueryAdapter.QueryFactory;
 
 public class BabyListActivity extends ActionBarActivity {
 
@@ -22,6 +32,7 @@ public class BabyListActivity extends ActionBarActivity {
 			getSupportFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
+
 	}
 
 	@Override
@@ -48,8 +59,17 @@ public class BabyListActivity extends ActionBarActivity {
 	 * A placeholder fragment containing a simple view.
 	 */
 	public static class PlaceholderFragment extends Fragment {
+		private ParseQueryAdapter<Favorite> mAdapter;
+		DisplayImageOptions options;
+		private ImageLoader imageLoader = ImageLoader.getInstance();
+		private ListView mList;
 
 		public PlaceholderFragment() {
+		}
+
+		@Override
+		public void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
 		}
 
 		@Override
@@ -57,8 +77,104 @@ public class BabyListActivity extends ActionBarActivity {
 				Bundle savedInstanceState) {
 			View rootView = inflater.inflate(R.layout.fragment_baby_list,
 					container, false);
+
+			mList = (ListView) rootView.findViewById(R.id.list);
+
 			return rootView;
 		}
+
+		@Override
+		public void onViewCreated(View view, Bundle savedInstanceState) {
+			super.onViewCreated(view, savedInstanceState);
+
+			doListQuery();
+
+		}
+
+		public void doListQuery() {
+
+			options = new DisplayImageOptions.Builder()
+					.showImageOnLoading(R.drawable.ic_launcher)
+					.showImageForEmptyUri(R.drawable.ic_launcher)
+					.showImageOnFail(R.drawable.ic_launcher)
+					.cacheInMemory(true).cacheOnDisc(true)
+					.considerExifParams(true)
+					.displayer(new RoundedBitmapDisplayer(20)).build();
+
+			ParseQueryAdapter.QueryFactory<Favorite> factory = getQueryFactory();
+
+			mAdapter = getParseQueryAdapter(factory);
+
+			// Disable automatic loading when the list_item_babysitter_comment
+			// is
+			// attached to a view.
+			mAdapter.setAutoload(false);
+
+			// Disable pagination, we'll manage the query limit ourselves
+			mAdapter.setPaginationEnabled(false);
+
+			mList.setAdapter(mAdapter);
+
+			mAdapter.loadObjects();
+		}
+
+		private ParseQueryAdapter.QueryFactory<Favorite> getQueryFactory() {
+			// Set up a customized query
+			ParseQueryAdapter.QueryFactory<Favorite> factory = new ParseQueryAdapter.QueryFactory<Favorite>() {
+				public ParseQuery<Favorite> create() {
+					// Location myLoc = (currentLocation == null) ? lastLocation
+					// :
+					// currentLocation;
+					ParseQuery<Favorite> query = Favorite.getQuery();
+					// query.include("user");
+					query.orderByDescending("createdAt");
+					// query.whereWithinKilometers("location",
+					// geoPointFromLocation(myLoc), radius * METERS_PER_FEET /
+					// METERS_PER_KILOMETER);
+					query.setLimit(20);
+					query.include("baby");
+					return query;
+				}
+			};
+			return factory;
+		}
+
+		private ParseQueryAdapter<Favorite> getParseQueryAdapter(
+				QueryFactory<Favorite> factory) {
+			ParseQueryAdapter<Favorite> adapter;
+			// Set up the query list_item_babysitter_comment
+			adapter = new ParseQueryAdapter<Favorite>(getActivity(), factory) {
+				@Override
+				public View getItemView(Favorite post, View view,
+						ViewGroup parent) {
+					if (view == null) {
+						view = View.inflate(getContext(),
+								R.layout.list_item_babysitter_list, null);
+					}
+					TextView babysitterName = (TextView) view
+							.findViewById(R.id.babysitter_name);
+					TextView babysitterAddress = (TextView) view
+							.findViewById(R.id.babysitter_address);
+
+					ImageView babysitterImage = (ImageView) view
+							.findViewById(R.id.babysitter_avator);
+
+					Baby baby = post.getBaby();
+
+					babysitterName.setText(baby.getName());
+					babysitterAddress.setText(baby.getNote());
+					imageLoader
+							.displayImage(
+									"https://fbcdn-sphotos-d-a.akamaihd.net/hphotos-ak-ash3/t1.0-9/q77/s720x720/1966891_782022338479354_124097698_n.jpg",
+									babysitterImage, options, null);
+
+					return view;
+				}
+
+			};
+			return adapter;
+		}
+
 	}
 
 }
--- src/tw/tasker/babysitter/view/BabyDetailActivity.java
+++ src/tw/tasker/babysitter/view/BabyDetailActivity.java
@@ -393,7 +393,7 @@ public class BabyDetailActivity extends ActionBarActivity implements
 						isInitData = false;
 					}else {
 						mStar.setChecked(true);
-						
+						mFavorite = favorite;
 					}
 					
 				}
