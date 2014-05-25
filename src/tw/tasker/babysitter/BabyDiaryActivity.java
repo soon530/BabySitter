594b1fbc278eab8c7938a21a928ad2ea78124c09
soon530 <soon530@gmail.com>
2014-05-25 16:21:48 +0800

    [寶寶日記]列出所有寶寶 OPEN - #27: 新增[寶寶列表]功能

--- src/tw/tasker/babysitter/BabyDiaryActivity.java
+++ src/tw/tasker/babysitter/BabyDiaryActivity.java
@@ -1,15 +1,24 @@
 package tw.tasker.babysitter;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import tw.tasker.babysitter.model.Baby;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.display.RoundedBitmapDisplayer;
+import com.parse.ParseQuery;
+import com.parse.ParseQueryAdapter;
+import com.parse.ParseQueryAdapter.QueryFactory;
 
 public class BabyDiaryActivity extends ActionBarActivity {
 
@@ -48,6 +57,10 @@ public class BabyDiaryActivity extends ActionBarActivity {
 	 * A placeholder fragment containing a simple view.
 	 */
 	public static class PlaceholderFragment extends Fragment {
+		private ParseQueryAdapter<Baby> mAdapter;
+		DisplayImageOptions options;
+		private ImageLoader imageLoader = ImageLoader.getInstance();
+		private ListView mList;
 
 		public PlaceholderFragment() {
 		}
@@ -55,10 +68,99 @@ public class BabyDiaryActivity extends ActionBarActivity {
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container,
 				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_baby_diary,
+			View rootView = inflater.inflate(R.layout.fragment_baby_list,
 					container, false);
+			mList = (ListView) rootView.findViewById(R.id.list);
 			return rootView;
 		}
-	}
 
+		@Override
+		public void onViewCreated(View view, Bundle savedInstanceState) {
+			super.onViewCreated(view, savedInstanceState);
+			doListQuery();
+		}
+
+		public void doListQuery() {
+
+			options = new DisplayImageOptions.Builder()
+					.showImageOnLoading(R.drawable.ic_launcher)
+					.showImageForEmptyUri(R.drawable.ic_launcher)
+					.showImageOnFail(R.drawable.ic_launcher)
+					.cacheInMemory(true).cacheOnDisc(true)
+					.considerExifParams(true)
+					.displayer(new RoundedBitmapDisplayer(20)).build();
+
+			ParseQueryAdapter.QueryFactory<Baby> factory = getQueryFactory();
+
+			mAdapter = getParseQueryAdapter(factory);
+
+			// Disable automatic loading when the list_item_babysitter_comment
+			// is
+			// attached to a view.
+			mAdapter.setAutoload(false);
+
+			// Disable pagination, we'll manage the query limit ourselves
+			mAdapter.setPaginationEnabled(false);
+
+			mList.setAdapter(mAdapter);
+
+			mAdapter.loadObjects();
+		}
+
+		private ParseQueryAdapter.QueryFactory<Baby> getQueryFactory() {
+			// Set up a customized query
+			ParseQueryAdapter.QueryFactory<Baby> factory = new ParseQueryAdapter.QueryFactory<Baby>() {
+				public ParseQuery<Baby> create() {
+					// Location myLoc = (currentLocation == null) ? lastLocation
+					// :
+					// currentLocation;
+					ParseQuery<Baby> query = Baby.getQuery();
+					// query.include("user");
+					query.orderByDescending("createdAt");
+					// query.whereWithinKilometers("location",
+					// geoPointFromLocation(myLoc), radius * METERS_PER_FEET /
+					// METERS_PER_KILOMETER);
+					query.setLimit(20);
+					query.include("baby");
+					return query;
+				}
+			};
+			return factory;
+		}
+
+		private ParseQueryAdapter<Baby> getParseQueryAdapter(
+				QueryFactory<Baby> factory) {
+			ParseQueryAdapter<Baby> adapter;
+			// Set up the query list_item_babysitter_comment
+			adapter = new ParseQueryAdapter<Baby>(getActivity(), factory) {
+				@Override
+				public View getItemView(Baby baby, View view,
+						ViewGroup parent) {
+					if (view == null) {
+						view = View.inflate(getContext(),
+								R.layout.list_item_babysitter_list, null);
+					}
+					TextView babysitterName = (TextView) view
+							.findViewById(R.id.babysitter_name);
+					TextView babysitterAddress = (TextView) view
+							.findViewById(R.id.babysitter_address);
+
+					ImageView babysitterImage = (ImageView) view
+							.findViewById(R.id.babysitter_avator);
+
+					babysitterName.setText(baby.getName());
+					babysitterAddress.setText(baby.getNote());
+					imageLoader
+							.displayImage(
+									"https://fbcdn-sphotos-d-a.akamaihd.net/hphotos-ak-ash3/t1.0-9/q77/s720x720/1966891_782022338479354_124097698_n.jpg",
+									babysitterImage, options, null);
+
+					return view;
+				}
+
+			};
+			return adapter;
+		}
+
+	}
 }
