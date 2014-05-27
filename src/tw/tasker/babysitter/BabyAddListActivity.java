b7ba93e9421abd73f49fb73bf25eae65e4547314
soon530 <soon530@gmail.com>
2014-05-27 17:06:03 +0800

    該保母下的所有寶寶日記列表 OPEN - #31: [新增寶寶日記] 爸媽 or 保母可以新增寶寶日記

--- src/tw/tasker/babysitter/BabyAddListActivity.java
+++ src/tw/tasker/babysitter/BabyAddListActivity.java
@@ -1,5 +1,19 @@
 package tw.tasker.babysitter;
 
+import java.util.List;
+
+import tw.tasker.babysitter.model.Baby;
+import tw.tasker.babysitter.view.BabyDetailActivity;
+
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.display.RoundedBitmapDisplayer;
+import com.parse.ParseQuery;
+import com.parse.ParseQueryAdapter;
+import com.parse.ParseUser;
+import com.parse.ParseQueryAdapter.OnQueryLoadListener;
+import com.parse.ParseQueryAdapter.QueryFactory;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -9,6 +23,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.AdapterView.OnItemClickListener;
 
 public class BabyAddListActivity extends ActionBarActivity {
 
@@ -59,7 +78,13 @@ public class BabyAddListActivity extends ActionBarActivity {
 	 * A placeholder fragment containing a simple view.
 	 */
 	public static class PlaceholderFragment extends Fragment {
-		private String mBabysitterObjectId;
+		private ParseQueryAdapter<Baby> mAdapter;
+		DisplayImageOptions options;
+		private ImageLoader imageLoader = ImageLoader.getInstance();
+		private ListView mList;
+		private TextView mEmpty;
+		String mBabysitterObjectId;
+		
 		public PlaceholderFragment(String babysitterObjectId) {
 			mBabysitterObjectId = babysitterObjectId;
 		}
@@ -67,10 +92,155 @@ public class BabyAddListActivity extends ActionBarActivity {
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container,
 				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_baby_add_list,
+			View rootView = inflater.inflate(R.layout.fragment_baby_list,
 					container, false);
+			mList = (ListView) rootView.findViewById(R.id.list);
+			
+			mList.setOnItemClickListener(new OnItemClickListener() {
+
+				@Override
+				public void onItemClick(AdapterView<?> parent, View view,
+						int position, long id) {
+					
+					Baby baby = mAdapter.getItem(position);
+					//Baby baby = favorite.getBaby();
+					
+					seeBabyDetail(baby.getObjectId());
+				}
+			
+			});
+			
+			mEmpty = (TextView) rootView.findViewById(R.id.empty);
+			mList.setEmptyView(mEmpty);
+
 			return rootView;
 		}
+		
+		public void seeBabyDetail(String babyObjectId) {
+			Bundle bundle = new Bundle();
+			bundle.putString("objectId", babyObjectId);
+			Intent intent = new Intent();
+			intent.putExtras(bundle);
+			intent.setClass(getActivity(), BabyDetailActivity.class);
+			startActivity(intent);
+		}
+
+
+		@Override
+		public void onViewCreated(View view, Bundle savedInstanceState) {
+			super.onViewCreated(view, savedInstanceState);
+			doListQuery();
+			
+		}
+		
+		public void doListQuery() {
+
+			options = new DisplayImageOptions.Builder()
+					.showImageOnLoading(R.drawable.ic_launcher)
+					.showImageForEmptyUri(R.drawable.ic_launcher)
+					.showImageOnFail(R.drawable.ic_launcher)
+					.cacheInMemory(true).cacheOnDisc(true)
+					.considerExifParams(true)
+					.displayer(new RoundedBitmapDisplayer(20)).build();
+
+			ParseQueryAdapter.QueryFactory<Baby> factory = getQueryFactory();
+
+			mAdapter = getParseQueryAdapter(factory);
+
+			// Disable automatic loading when the list_item_babysitter_comment
+			// is
+			// attached to a view.
+			mAdapter.setAutoload(false);
+
+			// Disable pagination, we'll manage the query limit ourselves
+			mAdapter.setPaginationEnabled(false);
+
+			mList.setAdapter(mAdapter);
+
+			mAdapter.loadObjects();
+			
+			mAdapter.addOnQueryLoadListener(new OnQueryLoadListener<Baby>() {
+
+				@Override
+				public void onLoaded(List<Baby> arg0, Exception arg1) {
+					getActivity().setProgressBarIndeterminateVisibility(false);					
+				}
+
+				@Override
+				public void onLoading() {
+					// TODO Auto-generated method stub
+					
+				}
+			});
+		}
+
+		private ParseQueryAdapter.QueryFactory<Baby> getQueryFactory() {
+			// Set up a customized query
+			ParseQueryAdapter.QueryFactory<Baby> factory = new ParseQueryAdapter.QueryFactory<Baby>() {
+				public ParseQuery<Baby> create() {
+					// Location myLoc = (currentLocation == null) ? lastLocation
+					// :
+					// currentLocation;
+					ParseQuery<Baby> query = Baby.getQuery();
+					// query.include("user");
+					query.orderByDescending("createdAt");
+					// query.whereWithinKilometers("location",
+					// geoPointFromLocation(myLoc), radius * METERS_PER_FEET /
+					// METERS_PER_KILOMETER);
+					query.setLimit(20);
+					query.include("baby");
+					
+					//query.whereEqualTo("user", ParseUser.getCurrentUser());
+					query.whereEqualTo("babysitterId", mBabysitterObjectId);
+					
+					return query;
+				}
+			};
+			return factory;
+		}
+
+		private ParseQueryAdapter<Baby> getParseQueryAdapter(
+				QueryFactory<Baby> factory) {
+			ParseQueryAdapter<Baby> adapter;
+			// Set up the query list_item_babysitter_comment
+			adapter = new ParseQueryAdapter<Baby>(getActivity(), factory) {
+				@Override
+				public View getItemView(Baby baby, View view,
+						ViewGroup parent) {
+					if (view == null) {
+						view = View.inflate(getContext(),
+								R.layout.list_item_babysitter_list, null);
+					}
+					TextView babysitterName = (TextView) view
+							.findViewById(R.id.babysitter_name);
+					TextView babysitterAddress = (TextView) view
+							.findViewById(R.id.babysitter_address);
+
+					ImageView babysitterImage = (ImageView) view
+							.findViewById(R.id.babysitter_avator);
+
+					babysitterName.setText(baby.getName());
+					babysitterAddress.setText(baby.getNote());
+					
+					String url;
+					if(baby.getPhotoFile() != null) {
+						url = baby.getPhotoFile().getUrl();
+					} else {
+						url = "https://fbcdn-sphotos-d-a.akamaihd.net/hphotos-ak-ash3/t1.0-9/q77/s720x720/1966891_782022338479354_124097698_n.jpg";
+					}
+					
+					imageLoader
+							.displayImage(
+									url,
+									babysitterImage, options, null);
+
+					return view;
+				}
+
+			};
+			return adapter;
+		}
+
 	}
 
 }
