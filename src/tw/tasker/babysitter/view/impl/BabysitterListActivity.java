8b8421e1b953b2e527813408dc8e062e93187c6f
soon530 <soon530@gmail.com>
2014-05-30 13:10:37 +0800

    本來只要調activity_babysitter_list.xml中的listview，後來把不需要的code都砍一砍 OPEN - #15: 重構 - MVP架構及Clean Code守則

--- src/tw/tasker/babysitter/view/impl/BabysitterListActivity.java
+++ src/tw/tasker/babysitter/view/impl/BabysitterListActivity.java
@@ -1,10 +1,6 @@
 package tw.tasker.babysitter.view.impl;
 
 import tw.tasker.babysitter.R;
-import tw.tasker.babysitter.R.id;
-import tw.tasker.babysitter.R.layout;
-import tw.tasker.babysitter.R.menu;
-import tw.tasker.babysitter.view.impl.BabysitterListFragment.Callbacks;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
@@ -12,90 +8,25 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
 
-/**
- * An activity representing a list of Items. This activity has different
- * presentations for handset and tablet-size devices. On handsets, the activity
- * presents a list of items, which when touched, lead to a
- * {@link BabysitterDetailActivity} representing item details. On tablets, the
- * activity presents the list of items and item details side-by-side using two
- * vertical panes.
- * <p>
- * The activity makes heavy use of fragments. The list of items is a
- * {@link BabysitterListFragment} and the item details (if present) is a
- * {@link BabysitterDetailFragment}.
- * <p>
- * This activity also implements the required {@link BabysitterListFragment.Callbacks}
- * interface to listen for item selections.
- */
-public class BabysitterListActivity extends ActionBarActivity implements
-		BabysitterListFragment.Callbacks {
-
-	/**
-	 * Whether or not the activity is in two-pane mode, i.e. running on a tablet
-	 * device.
-	 */
-	private boolean mTwoPane;
-
-	
+public class BabysitterListActivity extends ActionBarActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		/** Enabling Progress bar for this activity */
 		getWindow().requestFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
 
 		setContentView(R.layout.activity_babysitter_list);
 
-		if (findViewById(R.id.babysitter_detail_container) != null) {
-			// The detail container view will be present only in the
-			// large-screen layouts (res/values-large and
-			// res/values-sw600dp). If this view is present, then the
-			// activity should be in two-pane mode.
-			mTwoPane = true;
-
-			// In two-pane mode, list items should be given the
-			// 'activated' state when touched.
-			((BabysitterListFragment) getSupportFragmentManager().findFragmentById(
-					R.id.item_list)).setActivateOnItemClick(true);
-		}
-
-		// TODO: If exposing deep links into your app, handle intents here.
 	}
 
-	/**
-	 * Callback method from {@link BabysitterListFragment.Callbacks} indicating that
-	 * the item with the given ID was selected.
-	 */
-	@Override
-	public void onItemSelected(String id) {
-		if (mTwoPane) {
-			// In two-pane mode, show the detail view in this activity by
-			// adding or replacing the detail fragment using a
-			// fragment transaction.
-			Bundle arguments = new Bundle();
-			arguments.putString(BabysitterDetailFragment.ARG_ITEM_ID, id);
-			BabysitterDetailFragment fragment = new BabysitterDetailFragment();
-			fragment.setArguments(arguments);
-			getSupportFragmentManager().beginTransaction()
-					.replace(R.id.babysitter_detail_container, fragment).commit();
-
-		} else {
-			// In single-pane mode, simply start the detail activity
-			// for the selected item ID.
-			Intent detailIntent = new Intent(this, BabysitterDetailActivity.class);
-			detailIntent.putExtra(BabysitterDetailFragment.ARG_ITEM_ID, id);
-			startActivity(detailIntent);
-		}
-	}
-	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
 		getMenuInflater().inflate(R.menu.babysitter_list, menu);
-		//return true;
 		return super.onCreateOptionsMenu(menu);
 	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 
@@ -105,7 +36,7 @@ public class BabysitterListActivity extends ActionBarActivity implements
 			intent.setClass(this, BabysitterMapActivity.class);
 			startActivity(intent);
 		}
-		
+
 		return super.onOptionsItemSelected(item);
 	}
 }
--- src/tw/tasker/babysitter/view/impl/BabysitterListFragment.java
+++ src/tw/tasker/babysitter/view/impl/BabysitterListFragment.java
@@ -1,14 +1,20 @@
 package tw.tasker.babysitter.view.impl;
 
+import tw.tasker.babysitter.R;
 import tw.tasker.babysitter.presenter.BabysitterListPresenter;
 import tw.tasker.babysitter.presenter.impl.BabysitterListPresenterImpl;
 import tw.tasker.babysitter.view.BabysitterListView;
 import android.app.Activity;
+import android.app.Fragment;
 import android.os.Bundle;
-import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 /**
  * A list fragment representing a list of Items. This fragment also supports
@@ -19,141 +25,38 @@ import android.widget.ListView;
  * Activities containing this fragment MUST implement the {@link Callbacks}
  * interface.
  */
-public class BabysitterListFragment extends ListFragment implements
-		BabysitterListView {
+public class BabysitterListFragment extends Fragment implements
+		BabysitterListView, OnItemClickListener {
 
-	/**
-	 * The serialization (saved instance state) Bundle key representing the
-	 * activated item position. Only used on tablets.
-	 */
-	private static final String STATE_ACTIVATED_POSITION = "activated_position";
-
-	/**
-	 * The fragment's current callback object, which is notified of list item
-	 * clicks.
-	 */
-	private Callbacks mCallbacks = sDummyCallbacks;
-
-	/**
-	 * The current activated item position. Only used on tablets.
-	 */
-	private int mActivatedPosition = ListView.INVALID_POSITION;
 	private BabysitterListPresenter mPresenter;
 
-	private View mHeaderView;
-
-	/**
-	 * A callback interface that all activities containing this fragment must
-	 * implement. This mechanism allows activities to be notified of item
-	 * selections.
-	 */
-	public interface Callbacks {
-		/**
-		 * Callback for when an item has been selected.
-		 */
-		public void onItemSelected(String id);
-	}
-
-	/**
-	 * A dummy implementation of the {@link Callbacks} interface that does
-	 * nothing. Used only when this fragment is not attached to an activity.
-	 */
-	private static Callbacks sDummyCallbacks = new Callbacks() {
-		@Override
-		public void onItemSelected(String id) {
-		}
-	};
-
-	/**
-	 * Mandatory empty constructor for the fragment manager to instantiate the
-	 * fragment (e.g. upon screen orientation changes).
-	 */
-	public BabysitterListFragment() {
-	}
+	private ListView mList;
+	private TextView mEmpty;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+				
 		mPresenter = new BabysitterListPresenterImpl(this);
-		mPresenter.onCreate();
 	}
 
 	@Override
-	public void onResume() {
-		super.onResume();
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fragment_baby_list,
+				container, false);
+		mList = (ListView) rootView.findViewById(R.id.list);
+		mList.setOnItemClickListener(this);
+		mEmpty = (TextView) rootView.findViewById(R.id.empty);
+		mList.setEmptyView(mEmpty);
+		
+		return rootView;
 	}
-
-	
 	
 	@Override
 	public void onViewCreated(View view, Bundle savedInstanceState) {
 		super.onViewCreated(view, savedInstanceState);
-
-		// Restore the previously serialized activated item position.
-		if (savedInstanceState != null
-				&& savedInstanceState.containsKey(STATE_ACTIVATED_POSITION)) {
-			setActivatedPosition(savedInstanceState
-					.getInt(STATE_ACTIVATED_POSITION));
-		}
-	}
-
-	@Override
-	public void onAttach(Activity activity) {
-		super.onAttach(activity);
-
-		// Activities containing this fragment must implement its callbacks.
-		if (!(activity instanceof Callbacks)) {
-			throw new IllegalStateException(
-					"Activity must implement fragment's callbacks.");
-		}
-
-		mCallbacks = (Callbacks) activity;
-	}
-
-	@Override
-	public void onDetach() {
-		super.onDetach();
-
-		// Reset the active callbacks interface to the dummy implementation.
-		mCallbacks = sDummyCallbacks;
-	}
-
-	@Override
-	public void onListItemClick(ListView listView, View view, int position,
-			long id) {
-		super.onListItemClick(listView, view, position, id);
-		mPresenter.onListItemClick(position);
-	}
-
-	@Override
-	public void onSaveInstanceState(Bundle outState) {
-		super.onSaveInstanceState(outState);
-		if (mActivatedPosition != ListView.INVALID_POSITION) {
-			// Serialize and persist the activated item position.
-			outState.putInt(STATE_ACTIVATED_POSITION, mActivatedPosition);
-		}
-	}
-
-	/**
-	 * Turns on activate-on-click mode. When this mode is on, list items will be
-	 * given the 'activated' state when touched.
-	 */
-	public void setActivateOnItemClick(boolean activateOnItemClick) {
-		// When setting CHOICE_MODE_SINGLE, ListView will automatically
-		// give items the 'activated' state when touched.
-		getListView().setChoiceMode(
-				activateOnItemClick ? ListView.CHOICE_MODE_SINGLE
-						: ListView.CHOICE_MODE_NONE);
-	}
-
-	private void setActivatedPosition(int position) {
-		if (position == ListView.INVALID_POSITION) {
-			getListView().setItemChecked(mActivatedPosition, false);
-		} else {
-			getListView().setItemChecked(position, true);
-		}
-
-		mActivatedPosition = position;
+		mPresenter.onCreate();
 	}
 
 	@Override
@@ -168,6 +71,12 @@ public class BabysitterListFragment extends ListFragment implements
 
 	@Override
 	public void setAdapter(BaseAdapter adapter) {
-		setListAdapter(adapter);
+		mList.setAdapter(adapter);
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position,
+			long id) {
+		mPresenter.onListItemClick(position);
 	}
 }
