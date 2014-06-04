fd49fe8991dbacf6f60ea60292153dd34c6faf1c
soon530 <soon530@gmail.com>
2014-06-04 11:20:50 +0800

    [寶寶收藏] 可以更新資料 OPEN - #32: [UI/UX] 讓使用者可以更新資料

--- src/tw/tasker/babysitter/view/impl/FavoriteBabyActivity.java
+++ src/tw/tasker/babysitter/view/impl/FavoriteBabyActivity.java
@@ -14,6 +14,7 @@ import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -41,26 +42,6 @@ public class FavoriteBabyActivity extends ActionBarActivity {
 
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.baby_list, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
 	/**
 	 * A placeholder fragment containing a simple view.
 	 */
@@ -76,6 +57,31 @@ public class FavoriteBabyActivity extends ActionBarActivity {
 		@Override
 		public void onCreate(Bundle savedInstanceState) {
 			super.onCreate(savedInstanceState);
+		
+			setHasOptionsMenu(true);
+		}
+
+		@Override
+		public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+			inflater.inflate(R.menu.baby_list, menu);
+			super.onCreateOptionsMenu(menu, inflater);
+		}
+
+		@Override
+		public boolean onOptionsItemSelected(MenuItem item) {
+			// Handle action bar item clicks here. The action bar will
+			// automatically handle clicks on the Home/Up button, so long
+			// as you specify a parent activity in AndroidManifest.xml.
+			int id = item.getItemId();
+			
+			switch (id) {
+			case R.id.refresh:
+				mAdapter.loadObjects();
+			default:
+				break;
+			}
+			
+			return super.onOptionsItemSelected(item);
 		}
 
 		@Override
