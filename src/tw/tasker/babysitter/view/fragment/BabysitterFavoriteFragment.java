87a1cf705bfe85977fe1558af49843b45ea78de8
soon530 <soon530@gmail.com>
2014-06-08 23:34:29 +0800

    [保母收藏] 導入成功 OPEN - #39: [UI/UX] 導入ActionBarPullToRefresh

--- src/tw/tasker/babysitter/view/fragment/BabysitterFavoriteFragment.java
+++ src/tw/tasker/babysitter/view/fragment/BabysitterFavoriteFragment.java
@@ -9,6 +9,9 @@ import tw.tasker.babysitter.model.data.BabysitterFavorite;
 import tw.tasker.babysitter.presenter.adapter.BabysitterFavoriteParseQueryAdapter;
 import tw.tasker.babysitter.utils.ProgressBarUtils;
 import tw.tasker.babysitter.view.activity.BabysitterActivity;
+import uk.co.senab.actionbarpulltorefresh.library.ActionBarPullToRefresh;
+import uk.co.senab.actionbarpulltorefresh.library.PullToRefreshLayout;
+import uk.co.senab.actionbarpulltorefresh.library.listeners.OnRefreshListener;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -27,11 +30,12 @@ import com.parse.ParseQueryAdapter;
 import com.parse.ParseQueryAdapter.OnQueryLoadListener;
 
 public class BabysitterFavoriteFragment extends Fragment implements
-		OnItemClickListener, OnQueryLoadListener<BabysitterFavorite> {
+		OnItemClickListener, OnQueryLoadListener<BabysitterFavorite>, OnRefreshListener {
 	private ParseQueryAdapter<BabysitterFavorite> mAdapter;
 	private GridView mList;
 	private TextView mEmpty;
-	
+	private PullToRefreshLayout mPullToRefreshLayout;
+
 	public static Fragment newInstance(int position) {
 		BabysitterFavoriteFragment fragment = new BabysitterFavoriteFragment();
 		return fragment;
@@ -76,6 +80,19 @@ public class BabysitterFavoriteFragment extends Fragment implements
 		mEmpty = (TextView) rootView.findViewById(R.id.empty);
 		mList.setEmptyView(mEmpty);
 
+		// Retrieve the PullToRefreshLayout from the content view
+		mPullToRefreshLayout = (PullToRefreshLayout) rootView
+				.findViewById(R.id.carddemo_extra_ptr_layout);
+
+		// Now setup the PullToRefreshLayout
+		ActionBarPullToRefresh.from(getActivity())
+		// Mark All Children as pullable
+				.allChildrenArePullable()
+				// Set the OnRefreshListener
+				.listener(this)
+				// Finally commit the setup to our PullToRefreshLayout
+				.setup(mPullToRefreshLayout);
+
 		return rootView;
 	}
 
@@ -103,6 +120,7 @@ public class BabysitterFavoriteFragment extends Fragment implements
 	@Override
 	public void onLoaded(List<BabysitterFavorite> arg0, Exception arg1) {
 		ProgressBarUtils.hide(getActivity());
+		mPullToRefreshLayout.setRefreshComplete();
 	}
 
 	@Override
@@ -120,5 +138,10 @@ public class BabysitterFavoriteFragment extends Fragment implements
 		intent.putExtras(bundle);
 		intent.setClass(getActivity(), BabysitterActivity.class);
 		startActivity(intent);
+	}
+
+	@Override
+	public void onRefreshStarted(View v) {
+		mAdapter.loadObjects();
 	}
 }
