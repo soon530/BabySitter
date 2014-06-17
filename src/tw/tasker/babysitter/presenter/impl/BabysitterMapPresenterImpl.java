1c88fe7303c06d3560e3591045195c7080c7608a
soon530 <soon530@gmail.com>
2014-06-17 15:02:27 +0800

    將Model層從Presenter層移除 OPEN - #51: [保母地圖] 導入Clustering

--- src/tw/tasker/babysitter/presenter/impl/BabysitterMapPresenterImpl.java
+++ src/tw/tasker/babysitter/presenter/impl/BabysitterMapPresenterImpl.java
@@ -5,85 +5,87 @@ import static tw.tasker.babysitter.utils.LogUtils.makeLogTag;
 import java.util.List;
 
 import tw.tasker.babysitter.Config;
-import tw.tasker.babysitter.R;
-import tw.tasker.babysitter.model.BabysitterMapModel;
-import tw.tasker.babysitter.model.OnFinishedListener;
 import tw.tasker.babysitter.model.data.Babysitter;
-import tw.tasker.babysitter.model.impl.BabysitterMapModelImpl;
 import tw.tasker.babysitter.presenter.BabysitterMapPresenter;
 import tw.tasker.babysitter.utils.MyLocation;
 import tw.tasker.babysitter.view.activity.BabysitterActivity;
 import tw.tasker.babysitter.view.activity.BabysitterMapActivity;
-import tw.tasker.babysitter.view.activity.BabysittersActivity;
 import android.content.Intent;
 import android.location.Location;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseGeoPoint;
+import com.parse.ParseQuery;
 
-public class BabysitterMapPresenterImpl implements BabysitterMapPresenter, OnFinishedListener {
+public class BabysitterMapPresenterImpl extends FindCallback<Babysitter> implements BabysitterMapPresenter {
 	private static final String TAG = makeLogTag(BabysitterMapPresenterImpl.class);
 
-	private BabysitterMapActivity mBabysitterMap;
+	private BabysitterMapActivity mView;
 	private MyLocation mMyLocation;
-	private BabysitterMapModel mMapModel;
 
+	private static final int MAX_POST_SEARCH_RESULTS = 20;
+	// Maximum post search radius for map in kilometers
+	private static final int MAX_POST_SEARCH_DISTANCE = 100;
+
+	
 	public BabysitterMapPresenterImpl(
 			BabysitterMapActivity babysitterMapActivity) {
-		mBabysitterMap = babysitterMapActivity;
-		mMapModel = new BabysitterMapModelImpl(this);
+		mView = babysitterMapActivity;
 
-		mMyLocation = new MyLocation(mBabysitterMap);
+		mMyLocation = new MyLocation(mView);
 	}
 
 	@Override
 	public void onMapLoaded() {
-		mBabysitterMap.showMyLocation(mMyLocation.getmBounds());
+		mView.showMyLocation(mMyLocation.getmBounds());
 		doMapQuery();
 	}
 
 	private void doMapQuery() {
 		Location myLoc = mMyLocation.getCurLocation();
 
-		mMapModel.doMapQuery(myLoc);
+		doMapQuery(myLoc);
+	}
+	
+	
+	public void doMapQuery(Location myLoc) {
+		final ParseGeoPoint myPoint = geoPointFromLocation(myLoc);
+
+		ParseQuery<Babysitter> mapQuery = Babysitter.getQuery();
+		// Set up additional query filters
+		//.whereWithinKilometers("location", myPoint, MAX_POST_SEARCH_DISTANCE);
+		// mapQuery.include("user");
+		mapQuery.orderByDescending("createdAt");
+		//mapQuery.setLimit(MAX_POST_SEARCH_RESULTS);
+		// mapQuery.fromLocalDatastore();
+		// Kick off the query in the background
+		mapQuery.findInBackground(this);
+		//mapQuery.whereWithinGeoBox(key, southwest, northeast)
+		
+	}
+
+	/*
+	 * Helper method to get the Parse GEO point representation of a location
+	 */
+	private ParseGeoPoint geoPointFromLocation(Location loc) {
+		return new ParseGeoPoint(loc.getLatitude(), loc.getLongitude());
 	}
 
 	@Override
-	public void onFinished(List<MarkerOptions> markerOptions) {
-		mBabysitterMap.showMarkers(markerOptions);
+	public void done(List<Babysitter> babysitters, ParseException arg1) {
+		mView.AddMarkers(babysitters);
 	}
 	
-	@Override
-	public void onDataFinished(List<Babysitter> outlines) {
-		mBabysitterMap.AddMarkers(outlines);
-	}
-	
-	
-	@Override
-	public void onOptionsItemSelected(int id) {
-		if (id == R.id.action_list) {
-			Intent intent = new Intent();
-			intent.setClass(mBabysitterMap, BabysittersActivity.class);
-			mBabysitterMap.startActivity(intent);
-		}
-	}
-
 	@Override
 	public void onInfoWindowClick(Marker marker) {
 		final Intent intent = new Intent();
-		intent.setClass(mBabysitterMap, BabysitterActivity.class);
+		intent.setClass(mView, BabysitterActivity.class);
 		Bundle bundle = new Bundle();
 		bundle.putString(Config.BABYSITTER_OBJECT_ID, marker.getTitle());
-		bundle.putString("slat", String.valueOf(marker.getPosition().latitude));
-		bundle.putString("slng", String.valueOf(marker.getPosition().longitude));
-
-		bundle.putString("dlat", String.valueOf(mMyLocation.getLat()));
-		bundle.putString("dlng", String.valueOf(mMyLocation.getLng()));
-		
 		intent.putExtras(bundle);
-		mBabysitterMap.startActivity(intent);
+		mView.startActivity(intent);
 	}
-
-
 }
