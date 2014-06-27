7d045d6d372848f6144652388acb522f43955680
soon530 <soon530@gmail.com>
2014-06-27 16:41:09 +0800

    移除按menu hard key的選單 OPEN - #36: [UI/UX] 首頁選單導入color card

--- src/tw/tasker/babysitter/view/activity/HomeActivity.java
+++ src/tw/tasker/babysitter/view/activity/HomeActivity.java
@@ -8,9 +8,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class HomeActivity extends ActionBarActivity {
-	private static final String[] mSearchCondition = new String[] { "附近保母",
-			"寶寶日記","寶寶收藏", "保母收藏", "登出" };
-	//,"托育時段", "收托對象", "收托年齡", "可接受托育人數"
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -26,25 +23,5 @@ public class HomeActivity extends ActionBarActivity {
 		// 後續看要不要放在ActionBar之類的
 		//mUserInfo.setText("使用者資訊(" + user.getObjectId() + ")："+ user.getUsername() );
 
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.home, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
 	}
 }
