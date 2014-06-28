f1372d2306499ac58528927523b9417a3b61acc4
soon530 <soon530@gmail.com>
2014-06-28 21:39:09 +0800

    [保母地圖] 移除抓取[定位]功能，定位資料改由Home來取得 OPEN - #22: [UI/UX] 加入距離計算功能

--- src/tw/tasker/babysitter/presenter/impl/BabysitterMapPresenterImpl.java
+++ src/tw/tasker/babysitter/presenter/impl/BabysitterMapPresenterImpl.java
@@ -7,12 +7,10 @@ import java.util.List;
 import tw.tasker.babysitter.Config;
 import tw.tasker.babysitter.model.data.Babysitter;
 import tw.tasker.babysitter.presenter.BabysitterMapPresenter;
+import tw.tasker.babysitter.utils.LogUtils;
 import tw.tasker.babysitter.utils.MyLocation;
-import tw.tasker.babysitter.view.activity.BabysitterActivity;
 import tw.tasker.babysitter.view.activity.BabysitterMapActivity;
-import android.content.Intent;
 import android.location.Location;
-import android.os.Bundle;
 
 import com.google.android.gms.maps.model.Marker;
 import com.parse.FindCallback;
@@ -26,66 +24,38 @@ public class BabysitterMapPresenterImpl extends FindCallback<Babysitter> impleme
 	private BabysitterMapActivity mView;
 	private MyLocation mMyLocation;
 
-	private static final int MAX_POST_SEARCH_RESULTS = 20;
-	// Maximum post search radius for map in kilometers
-	private static final int MAX_POST_SEARCH_DISTANCE = 100;
-
-	
 	public BabysitterMapPresenterImpl(
 			BabysitterMapActivity babysitterMapActivity) {
 		mView = babysitterMapActivity;
-
-		mMyLocation = new MyLocation(mView);
 	}
 
 	@Override
 	public void onMapLoaded() {
-		mView.showMyLocation(mMyLocation.getmBounds());
 		doMapQuery();
+		LogUtils.LOGD("vic", "onMapLoaded..");
 	}
 
-	private void doMapQuery() {
-		Location myLoc = mMyLocation.getCurLocation();
-
-		doMapQuery(myLoc);
-	}
-	
-	
-	public void doMapQuery(Location myLoc) {
-		final ParseGeoPoint myPoint = geoPointFromLocation(myLoc);
+	public void doMapQuery() {
 
 		ParseQuery<Babysitter> mapQuery = Babysitter.getQuery();
-		// Set up additional query filters
-		//.whereWithinKilometers("location", myPoint, MAX_POST_SEARCH_DISTANCE);
-		// mapQuery.include("user");
-		mapQuery.orderByDescending("createdAt");
-		//mapQuery.setLimit(MAX_POST_SEARCH_RESULTS);
-		// mapQuery.fromLocalDatastore();
-		// Kick off the query in the background
+		mapQuery.whereWithinKilometers("location", Config.MY_LOCATION, Config.MAX_POST_SEARCH_DISTANCE);
 		mapQuery.findInBackground(this);
+		// mapQuery.fromLocalDatastore();
 		//mapQuery.whereWithinGeoBox(key, southwest, northeast)
-		
-	}
-
-	/*
-	 * Helper method to get the Parse GEO point representation of a location
-	 */
-	private ParseGeoPoint geoPointFromLocation(Location loc) {
-		return new ParseGeoPoint(loc.getLatitude(), loc.getLongitude());
 	}
 
 	@Override
 	public void done(List<Babysitter> babysitters, ParseException arg1) {
-		mView.AddMarkers(babysitters);
+		mView.AddMarkers(babysitters, Config.MY_LOCATION);
 	}
 	
 	@Override
 	public void onInfoWindowClick(Marker marker) {
-		final Intent intent = new Intent();
+/*		final Intent intent = new Intent();
 		intent.setClass(mView, BabysitterActivity.class);
 		Bundle bundle = new Bundle();
 		bundle.putString(Config.BABYSITTER_OBJECT_ID, marker.getTitle());
 		intent.putExtras(bundle);
 		mView.startActivity(intent);
-	}
+*/	}
 }
