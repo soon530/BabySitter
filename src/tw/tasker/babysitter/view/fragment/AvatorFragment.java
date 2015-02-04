00cdc222bd0e60aa627a8210e75ab4f939c686b0
soon530 <soon530@gmail.com>
2015-02-04 14:37:06 +0800

    Add a list dialog to select where avator from.

--- src/tw/tasker/babysitter/view/fragment/AvatorFragment.java
+++ src/tw/tasker/babysitter/view/fragment/AvatorFragment.java
@@ -1,13 +1,20 @@
 package tw.tasker.babysitter.view.fragment;
 
 import tw.tasker.babysitter.R;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
-public class AvatorFragment extends Fragment {
+public class AvatorFragment extends Fragment implements OnClickListener {
+	private ImageView mAvator;
+
 	public static Fragment newInstance() {
 		Fragment fragment = new AvatorFragment();
 
@@ -16,14 +23,28 @@ public class AvatorFragment extends Fragment {
 	}
 
 	public AvatorFragment() {
-		// Required empty public constructor
 	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		View rootView = inflater.inflate(R.layout.fragment_avator, container, false);
-
+		View rootView = inflater.inflate(R.layout.fragment_avator, container,
+				false);
+		mAvator = (ImageView) rootView.findViewById(R.id.avator);
+		mAvator.setOnClickListener(this);
 		return rootView;
+	}
+
+	@Override
+	public void onClick(View v) {
+		final String[] imageMethods = {"照相", "相簿"}; 
+		DialogFragment frag = new ListDialogFragment(imageMethods, new DialogInterface.OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				
+			}
+		});
+		frag.show(getFragmentManager(), "dialog");
 	}
 }
