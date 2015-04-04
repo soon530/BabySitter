9ff5dae9e6a73ff1e25cbcb8a0786783f19559af
soon530 <soon530@gmail.com>
2015-04-04 16:53:36 +0800

    Delete unnecessary code.

--- src/tw/tasker/babysitter/view/activity/SignUpActivity.java
+++ src/tw/tasker/babysitter/view/activity/SignUpActivity.java
@@ -3,7 +3,6 @@ package tw.tasker.babysitter.view.activity;
 import tw.tasker.babysitter.R;
 import tw.tasker.babysitter.view.fragment.SignUpParentFragment;
 import tw.tasker.babysitter.view.fragment.SyncDataFragment;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -14,14 +13,7 @@ import android.view.MenuItem;
 
 import com.astuetz.PagerSlidingTabStrip;
 
-/**
- * Activity which displays a login screen to the user.
- */
 public class SignUpActivity extends BaseActivity {
-	// UI references.
-//	private EditText usernameView;
-//	private EditText passwordView;
-//	private EditText passwordAgainView;
 	private PagerSlidingTabStrip tabs;
 	private ViewPager pager;
 	private MyPagerAdapter adapter;
@@ -42,100 +34,9 @@ public class SignUpActivity extends BaseActivity {
 		pager.setPageMargin(pageMargin);
 
 		tabs.setViewPager(pager);
-		//tabs.setIndicatorColor(currentColor);
 		
 		//getActionBar().setDisplayShowHomeEnabled(false);
-
 		//getActionBar().setDisplayHomeAsUpEnabled(true);
-
-//		setContentView(R.layout.activity_signup);
-//		getActionBar().setDisplayHomeAsUpEnabled(true);
-//
-//		// Set up the signup form.
-//		usernameView = (EditText) findViewById(R.id.username);
-//		passwordView = (EditText) findViewById(R.id.password);
-//		passwordAgainView = (EditText) findViewById(R.id.passwordAgain);
-//
-//		// Set up the submit button click handler
-//		findViewById(R.id.action_button).setOnClickListener(
-//				new View.OnClickListener() {
-//					public void onClick(View view) {
-//
-//						// Validate the sign up data
-//						boolean validationError = false;
-//						StringBuilder validationErrorMessage = new StringBuilder(
-//								getResources().getString(R.string.error_intro));
-//						if (isEmpty(usernameView)) {
-//							validationError = true;
-//							validationErrorMessage.append(getResources()
-//									.getString(R.string.error_blank_username));
-//						}
-//						if (isEmpty(passwordView)) {
-//							if (validationError) {
-//								validationErrorMessage.append(getResources()
-//										.getString(R.string.error_join));
-//							}
-//							validationError = true;
-//							validationErrorMessage.append(getResources()
-//									.getString(R.string.error_blank_password));
-//						}
-//						if (!isMatching(passwordView, passwordAgainView)) {
-//							if (validationError) {
-//								validationErrorMessage.append(getResources()
-//										.getString(R.string.error_join));
-//							}
-//							validationError = true;
-//							validationErrorMessage
-//									.append(getResources()
-//											.getString(
-//													R.string.error_mismatched_passwords));
-//						}
-//						validationErrorMessage.append(getResources().getString(
-//								R.string.error_end));
-//
-//						// If there is a validation error, display the error
-//						if (validationError) {
-//							Toast.makeText(SignUpActivity.this,
-//									validationErrorMessage.toString(),
-//									Toast.LENGTH_LONG).show();
-//							return;
-//						}
-//
-//						// Set up a progress dialog
-//						final ProgressDialog dlg = new ProgressDialog(
-//								SignUpActivity.this);
-//						dlg.setTitle("註冊中");
-//						dlg.setMessage("請稍候...");
-//						dlg.show();
-//
-//						// Set up a new Parse user
-//						ParseUser user = new ParseUser();
-//						user.setUsername(usernameView.getText().toString());
-//						user.setPassword(passwordView.getText().toString());
-//						// Call the Parse signup method
-//						user.signUpInBackground(new SignUpCallback() {
-//
-//							@Override
-//							public void done(ParseException e) {
-//								dlg.dismiss();
-//								if (e != null) {
-//									// Show the error message
-//									Toast.makeText(SignUpActivity.this,
-//											"註冊錯誤!" /* e.getMessage() */,
-//											Toast.LENGTH_LONG).show();
-//								} else {
-//									// Start an intent for the dispatch activity
-//									Intent intent = new Intent(
-//											SignUpActivity.this,
-//											DispatchActivity.class);
-//									intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK
-//											| Intent.FLAG_ACTIVITY_NEW_TASK);
-//									startActivity(intent);
-//								}
-//							}
-//						});
-//					}
-//				});
 	}
 
 	@Override
@@ -196,6 +97,4 @@ public class SignUpActivity extends BaseActivity {
 			return fragment;
 		}
 	}
-
-
 }
