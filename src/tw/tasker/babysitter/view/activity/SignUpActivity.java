fe12eb0de62d53ef98c3547f450237c2cc6c2c1f
soon530 <soon530@gmail.com>
2015-04-13 10:34:29 +0800

    Add menu into action bar.

--- src/tw/tasker/babysitter/view/activity/SignUpActivity.java
+++ src/tw/tasker/babysitter/view/activity/SignUpActivity.java
@@ -6,20 +6,31 @@ import tw.tasker.babysitter.view.fragment.CreateAccountFragment;
 import tw.tasker.babysitter.view.fragment.SignUpParentFragment;
 import tw.tasker.babysitter.view.fragment.SyncDataFragment;
 import tw.tasker.babysitter.view.fragment.VerifyCodeFragment;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.TypedValue;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.SubMenu;
 
 import com.astuetz.PagerSlidingTabStrip;
+import com.parse.ParseUser;
 
 public class SignUpActivity extends BaseActivity {
 	private PagerSlidingTabStrip tabs;
 	private ViewPager pager;
 	private MyPagerAdapter adapter;
+	private Object inflater;
+	private MenuItem mItem;
+	private SubMenu mSubMenu;
+	private MenuItem mLogoutItem;
+	private MenuItem mProfileItem;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -43,19 +54,89 @@ public class SignUpActivity extends BaseActivity {
 		// getActionBar().setDisplayShowHomeEnabled(false);
 		// getActionBar().setDisplayHomeAsUpEnabled(true);
 	}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		
+		MenuInflater inflater = getMenuInflater();
+		
+		inflater.inflate(R.menu.home, menu);
+
+		mItem = menu.findItem(R.id.action_settings);
+		mSubMenu = mItem.getSubMenu();
+		mLogoutItem = mSubMenu.findItem(R.id.action_logout);
+		mProfileItem = mSubMenu.findItem(R.id.action_profile);
+
+		if (ParseUser.getCurrentUser() == null) {
+			mLogoutItem.setTitle("登入");
+			mProfileItem.setVisible(false);
+
+		} else {
+			mLogoutItem.setTitle("登出");
+		}
+		
+		return super.onCreateOptionsMenu(menu);
+
+	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		int id = item.getItemId();
+		String uri = "";
+		Intent intent;
 
 		switch (id) {
 		case android.R.id.home:
 			finish();
 			break;
 
+		case R.id.action_profile:
+			intent = new Intent();
+			intent.setClass(this, ProfileActivity.class);
+			startActivity(intent);
+
+			break;
+
+		// case R.id.action_google_play:
+		// uri = "market://details?id=tw.tasker.babysitter";
+		// intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+		// startActivity(intent);
+		// break;
+		case R.id.action_fb:
+			uri = "fb://page/765766966779332";
+			intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+			startActivity(intent);
+			break;
+
+		case R.id.action_gmail:
+			intent = new Intent(Intent.ACTION_SEND);
+			intent.setType("message/rfc822");
+			intent.putExtra(Intent.EXTRA_EMAIL,
+					new String[] { "soon530@gmail.com" });
+			intent.putExtra(Intent.EXTRA_SUBJECT, "Search保母意見回饋");
+			// intent.putExtra(Intent.EXTRA_TEXT, "");
+			startActivity(Intent.createChooser(intent, "Search保母意見回饋"));
+			break;
+
+		case R.id.action_logout:
+			if (ParseUser.getCurrentUser() == null) { // 沒有登入
+			} else { // 有登入
+				// Call the Parse log out method
+				ParseUser.logOut();
+			}
+
+			intent = new Intent();
+			// Start and intent for the dispatch activity
+			intent.setClass(this, DispatchActivity.class);
+			intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK
+					| Intent.FLAG_ACTIVITY_NEW_TASK);
+			startActivity(intent);
+
+			break;
 		default:
 			break;
 		}
+
+		
 		return super.onOptionsItemSelected(item);
 	}
 
